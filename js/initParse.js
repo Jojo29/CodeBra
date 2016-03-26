4568933a4254d6211b264ac2128d7bff08ccc0e8
Monique Shotande <momoshog@ufl.edu>
2016-03-26 05:51:06 -0400

    create reviews

---
+++ js/initParse.js
@@ -0,0 +1,4 @@
+//Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY");
+Parse.initialize("gV5Y1LeLQZkp8ZlxZbPtO9bzuiSV8rDBAkxdV37a", "5z7IsSnkSjTp0k5kP8rJvbNQZTJapIyctjD62cZb");
+
+//Parse.serverURL = 'http://YOUR_PARSE_SERVER:1337/parse'
